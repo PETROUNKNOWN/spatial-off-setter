432fc717bdfd1dc124452f8a2acf10885ea66e8f
peet <petrounknown@gmail.com>
2025-03-07 21:14:34 +0300

    Updated Source

--- finalApp.py
+++ finalApp.py
@@ -14,6 +14,9 @@ class App(ctk.CTk):
         self.grid_size = 150
         self.grid_lines = "solid"
         self.file_path = "assets/oemTone.mp3"
+        self.sensors_delta = 0.5
+        self.sound_speed=343
+        self.meter_multiplier=1
 
         self.columnconfigure(0, weight=1)
         self.rowconfigure(0, weight=1)
@@ -27,7 +30,10 @@ class App(ctk.CTk):
         controlsFrame.grid_propagate(False)
         controlsFrame.grid_columnconfigure(1, weight=1)
 
-        self.populate_platformFrame(platformFrame)
+        self.canvas = ctk.CTkCanvas(platformFrame, width=600, height=600, bg="white")
+        self.canvas.pack(side="left", padx=10, pady=10)
+        self.canvas.bind("<Button-1>", self.click_handler)
+        self.draw_grid()
         self.populate_controlsFrame(controlsFrame)
         self.data, self.samplerate = sf.read(self.file_path, dtype='float32')
 
@@ -69,173 +75,74 @@ class App(ctk.CTk):
         self.draw_grid()
 
     def populate_platformFrame(self, frame):
-        self.canvas = ctk.CTkCanvas(frame, width=600, height=600, bg="white")
-        self.canvas.pack(side="left", padx=10, pady=10)
-        self.canvas.bind("<Button-1>", self.on_click)
-        self.draw_grid()
+        pass
+        
 
     def draw_grid(self):
         self.canvas.delete("all")
-        width = int(self.canvas.cget("width"))
-        height = int(self.canvas.cget("height"))
-        
-        # Draw axis
-        self.canvas.create_line(width // 2, 0, width // 2, height, fill="black", width=2)
-        self.canvas.create_line(0, height // 2, width, height // 2, fill="black", width=2)
-        self.dash_pattern = (2, 2) if self.grid_lines.lower() == "dashed" else None
-
-        # Draw grid lines
-        # step = self.grid_size
-        # for i in range(0, width, step):
-            
-        #     self.canvas.create_line(i, 0, i, height, fill="gray", dash=dash_pattern)
-        # for i in range(0, height, step):
-        #     self.canvas.create_line(0, i, width, i, fill="gray", dash=dash_pattern)
-
+        size=600
+        half_size=size//2
+        self.canvas.create_line(half_size,0,half_size,size,fill="black",width=2)
+        self.canvas.create_line(0,half_size,size,half_size,fill="black",width=2)
+        self.dash=(2,2) if self.grid_lines.lower()=="dashed" else None
         self.canvasDivisions=10
         self.canvasSize=600
-
         self.scaler=self.canvasSize/self.canvasDivisions
-
         self.canvasStep=self.canvasSize/self.canvasDivisions
-        for i in range(1, self.canvasDivisions):
-            for j in range(1, self.canvasDivisions):
-                x = i * self.canvasStep
-                y = j * self.canvasStep
-                self.canvas.create_line(x, 0, x, self.canvasSize, fill="black", dash=self.dash_pattern)
-                self.canvas.create_line(0, y, self.canvasSize, y, fill="black", dash=self.dash_pattern)
+        for i in range(1,self.canvasDivisions):
+            for j in range(1,self.canvasDivisions):
+                x=i*self.canvasStep
+                y=j*self.canvasStep
+                self.canvas.create_line(x,0,x,self.canvasSize,fill="black",dash=self.dash)
+                self.canvas.create_line(0,y,self.canvasSize,y,fill="black",dash=self.dash)
 
+        #Debug
         coords = [(300,300),(0,0),(300,0),(600,0),(300,0),(180,180),(420,180)]
         # for i in coords:
             # left_delay, right_delay, left_volume, right_volume = self.calculate_delay_and_volume(i[0], i[1])
+        #Debug
                 
+    def click_handler(self,event):
+        x,y=event.x,event.y
+        print(f"Click at {x},{y}")
+        l_del,r_del,l_vol,r_vol=self.adjustment_algo(x,y)
+        self.mixer_algo(l_del,r_del,l_vol,r_vol)
 
-    def on_click(self, event):
-        click_x, click_y = event.x, event.y
-        print(f"Click at {click_x},{click_y}")
-        
-        left_delay, right_delay, left_volume, right_volume = self.calculate_delay_and_volume(click_x, click_y)
-        # print(f"received data")
-        # print(f"starting audio change")
-        self.play_stereo_sound(left_delay, right_delay, left_volume, right_volume)
+    def mixer_algo(self,l_del,r_del,l_vol,r_vol):
+        l_del_samps=int(l_del*self.samplerate)
+        r_del_samps=int(r_del*self.samplerate)
+        l_channel=np.zeros((len(self.data)+l_del_samps,2))
+        r_channel=np.zeros((len(self.data)+r_del_samps,2))
+        l_channel[l_del_samps:l_del_samps+len(self.data),0]=self.data[:,0]*l_vol
+        r_channel[r_del_samps:r_del_samps+len(self.data),1]=self.data[:,1]*r_vol
+        combined_len=max(len(l_channel),len(r_channel))
+        mixed_signal=np.zeros((combined_len,2))
+        mixed_signal[:len(l_channel)]+=l_channel
+        mixed_signal[:len(r_channel)]+=r_channel
+        #Play audio
+        sd.play(mixed_signal, self.samplerate)
+        sd.wait()
 
-    def play_stereo_sound(self, left_delay, right_delay, left_volume, right_volume):
-        try:
-            # print(f"starting audio change")
-            # data, samplerate = sf.read(self.file_path, dtype='float32')
-
-            # Apply delay as samples
-            left_delay_samples = int(left_delay * self.samplerate)
-            right_delay_samples = int(right_delay * self.samplerate)
-            # print(f"done sampling audio")
-            # print(f"starting audio delay")
-            # Create delayed signals
-            left_channel = np.zeros((len(self.data) + left_delay_samples, 2))
-            right_channel = np.zeros((len(self.data) + right_delay_samples, 2))
-            # print(f"done audio delay")
-            # print(f"starting audio volume")
-
-            left_channel[left_delay_samples:left_delay_samples + len(self.data), 0] = self.data[:, 0] * left_volume
-            right_channel[right_delay_samples:right_delay_samples + len(self.data), 1] = self.data[:, 1] * right_volume
-            # print(f"done audio volume")
-            # print(f"starting audio mixing")
-            # Mix the channels
-            combined_length = max(len(left_channel), len(right_channel))
-            mixed_signal = np.zeros((combined_length, 2))
-            mixed_signal[:len(left_channel)] += left_channel
-            mixed_signal[:len(right_channel)] += right_channel
-            # print(f"done audio mixing")
-
-            # Stream the audio
-            # print(f"starting audio playing")
-            sd.play(mixed_signal, self.samplerate)
-            # print(f"still playing")
-            sd.wait()
-            # print(f"done audio mixing")
-        
-        except Exception as e:
-            print(f"Error playing sound: {e}")
-
-    def calculate_delay_and_volume(self, click_x, click_y):
-        try:
-            print(f"Coord:{click_x},{click_y}")
-            # we find the middle of the platform
-            # print(f"Doing coordinates")
-            center_x = int(self.canvas.cget("width")) // 2
-            center_y = int(self.canvas.cget("height")) // 2
-            x_dist = click_x - center_x
-            y_dist = click_y - center_y
-            # x_dist = x_dist * x_dist
-            # print(f"Doing squarerooting")
-            left_distance = math.sqrt((x_dist + 0.5 / 2)**2 + y_dist**2)
-            right_distance = math.sqrt((x_dist - 0.5 / 2)**2 + y_dist**2)
-            # print(f"Doing delaying")
-            # print(f"left_distance: {left_distance}")
-            # print(f"right_distance: {right_distance}")
-
-            
-            left_distance = left_distance / self.scaler
-            right_distance = right_distance / self.scaler
-
-
-
+    def adjustment_algo(self,x,y):
+        cen=int(self.canvas.cget("height"))//2
+        x_dist=x-cen
+        y_dist=y-cen
+        l_dist=(math.sqrt((x_dist+self.sensors_delta/2)**2+y_dist**2))/self.scaler
+        r_dist=(math.sqrt((x_dist-self.sensors_delta/2)**2+y_dist**2))/self.scaler
+        l_del=(l_dist/self.sound_speed)*self.meter_multiplier
+        r_del=(r_dist/self.sound_speed)*self.meter_multiplier
+        if l_del<r_del:
+            l_vol=1/(l_dist*0.5)
+            r_vol=1/(0.8+l_dist)       
+        elif l_del>r_del:
+            r_vol=1/(r_dist*0.5)
+            l_vol=1/(0.8+r_dist)
+        else:
+            l_vol=1
+            r_vol=1
             # volume for low doent work
             # time for high dont work
-            
-            left_delay = (left_distance / 343) * 1
-            right_delay = (right_distance / 343) * 1
-
-            # if left_delay<right_delay:
-            #     left_volume=1
-            #     # print(f"delays diffs:{right_delay - left_delay}")
-            #     # right_volume=(right_delay+left_delay) * 1
-            #     right_volume=1 / (0.4 + left_distance)
-            # elif left_delay>right_delay:
-            #     right_volume=1
-            #     # print(f"delays diffs:{left_delay - right_delay}")
-            #     # left_volume=(left_delay+right_delay) * 1
-            #     left_volume=1 / (0.4 + right_distance)
-            # else:
-            #     left_volume=1
-            #     right_volume=1
-
-
-            if left_delay < right_delay:
-                left_volume = 1 / (left_distance * 0.5)  # slower falloff for the primary side
-                right_volume = 1 / (0.8 + left_distance)       
-            elif left_delay > right_delay:
-                right_volume = 1 / (right_distance * 0.5)
-                left_volume = 1 / (0.8 + right_distance)
-            else:
-                left_volume = 1
-                right_volume = 1
-
-            #volume inverse distance
-            # left_volume = 1 / (0 + left_distance)
-            # right_volume = 1 / (0 + right_distance)
-
-
-
-
-            # left_delay=0.008000008 # sounds interesting
-            # left_delay=0.005
-            # right_delay=0
-            # volume_decay=0.34
-            # left_volume = 1 / (1 + left_distance)
-            # right_volume = 1 / (1 + right_distance)
-            # the delay needs to work with the volume, neither can be constant. Try finding the theta of said relation
-
-            # left_volume = 1
-            # right_volume = 1
-
-            # print(f"returning data")
-            print(f"left_delay: {left_delay}")
-            print(f"right_delay: {right_delay}")
-            print(f"left_volume: {left_volume}")
-            print(f"right_volume: {right_volume}")
-            return left_delay, right_delay, left_volume, right_volume
-        except Exception as e:
-            print(f"Error playing sound: {e}")
+        return l_del,r_del,l_vol,r_vol
 
 if __name__ == "__main__":
     app = App()
